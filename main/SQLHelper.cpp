acd35c6e149b2e7a29cedb282d838185ffb2588a
Fabio Paolini <fabio2paolini@gmail.com>
2016-10-25 09:25:38 -0200

    Portuguese language is set as default

--- main/SQLHelper.cpp
+++ main/SQLHelper.cpp
@@ -2785,7 +2785,7 @@ bool CSQLHelper::OpenDatabase()
 	}
 	if ((!GetPreferencesVar("Language", sValue)) || (sValue.empty()))
 	{
-		UpdatePreferencesVar("Language", "en");
+		UpdatePreferencesVar("Language", "pt");
 	}
 	if (!GetPreferencesVar("DashboardType", nValue))
 	{
