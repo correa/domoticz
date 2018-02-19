364ed63c894299ae51033b0ae5e87b6b1b687697
Erik <erik@montnemery.com>
2018-02-19 20:43:49 +0100

    Fix broken logic when submitting custom client ID to MQTT connect

--- hardware/plugins/PluginProtocols.cpp
+++ hardware/plugins/PluginProtocols.cpp
@@ -1436,7 +1436,7 @@ namespace Plugins {
 
 				// Client Identifier
 				PyObject *pID = PyDict_GetItemString(WriteMessage->m_Object, "ID");
-				if (pID && !PyUnicode_Check(pID))
+				if (pID && PyUnicode_Check(pID))
 				{
 					MQTTPushBackStringWLen(std::string(PyUnicode_AsUTF8(pID)), vPayload);
 				}
