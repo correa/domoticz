a4a874316ecbef7a836c41223629aee75aa7f04e
Erik <erik@montnemery.com>
2018-04-27 16:50:23 +0200

    Make group/scene add options non-inverted according to review comments.

--- main/SQLHelper.cpp
+++ main/SQLHelper.cpp
@@ -38,7 +38,7 @@
 #include <machine/int_fmtio.h>
 #endif
 
-#define DB_VERSION 126
+#define DB_VERSION 127
 
 extern http::server::CWebServerHelper m_webservers;
 extern std::string szWWWFolder;
@@ -2517,6 +2517,10 @@ bool CSQLHelper::OpenDatabase()
 		{
 			std::string sFile = szWWWFolder + "/js/domoticzdevices.js.gz";
 			std::remove(sFile.c_str());
+		}
+		if (dbversion < 127)
+		{
+			safe_query("UPDATE Hardware SET Mode2 = 3 WHERE Type = %d", HTYPE_Philips_Hue);
 		}
 	}
 	else if (bNewInstall)
